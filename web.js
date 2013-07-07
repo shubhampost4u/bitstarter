887f17ecdfbf0ee30d86351d024bb6f155c94d91
shubhampost4u <shubham.post4u@gmail.com>
2013-07-07 15:51:53 +0530

    modified web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   	
 	var buf = fs.readFileSync("index.html");
-	response.send(buf.toString);
+	response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
